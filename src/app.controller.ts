b941249c57f4ed5bfae1b5d2aa8c2c5054705919
Timo Peters <timoopeters@outlook.de>
2020-10-30 14:12:51 +0100

    added ping route

---
+++ src/app.controller.ts
@@ -0,0 +1,12 @@
+import { Controller, Get } from '@nestjs/common';
+
+@Controller('')
+export class AppController {
+    @Get("ping")
+    ping() {
+        return "pong";
+    }
+}
+
+
+
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { AppController } from './app.controller';
 import { ColorsModule } from './colors/colors.module';
 import { EspModule } from './esp/esp.module';
 import { Esp, EspSchema } from './schemas/esp.schema';
@@ -7,5 +8,6 @@ import { SettingsModule } from './settings/settings.module';
 
 @Module({
   imports: [EspModule, MongooseModule.forRoot("mongodb://localhost/devlight", { useFindAndModify: false }), MongooseModule.forFeature([{ name: Esp.name, schema: EspSchema }]), ColorsModule, SettingsModule],
+  controllers: [AppController],
 })
 export class AppModule { }
