a10623b8ed7f814ec2ab9be77c552da3ce06905e
Timo Peters <peters@teck-digital.de>
2021-01-05 14:10:44 +0100

    added config

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { ColorsModule } from './colors/colors.module';
 import { EspModule } from './esp/esp.module';
 import { Esp, EspSchema } from './schemas/esp.schema';
 import { SettingsModule } from './settings/settings.module';
+import {config} from "./config"
 
 @Module({
-  imports: [EspModule, MongooseModule.forRoot("mongodb://localhost/devlight", { useFindAndModify: false }), MongooseModule.forFeature([{ name: Esp.name, schema: EspSchema }]), ColorsModule, SettingsModule],
+  imports: [EspModule, MongooseModule.forRoot(config.database.url, { useFindAndModify: false }), MongooseModule.forFeature([{ name: Esp.name, schema: EspSchema }]), ColorsModule, SettingsModule],
   controllers: [AppController],
 })
 export class AppModule { }
