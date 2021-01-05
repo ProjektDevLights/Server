6d67d74b7f7093c98a0bdc84d36bf7a82bb83a75
3azygam3r04 <jaanfredder@gmail.com>
2021-01-05 18:23:07 +0100

    config import path gefixt

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,7 @@ import { ColorsModule } from './colors/colors.module';
 import { EspModule } from './esp/esp.module';
 import { Esp, EspSchema } from './schemas/esp.schema';
 import { SettingsModule } from './settings/settings.module';
-import {config} from "./config"
+import {config} from "./config.template"
 
 @Module({
   imports: [EspModule, MongooseModule.forRoot(config.database.url, { useFindAndModify: false }), MongooseModule.forFeature([{ name: Esp.name, schema: EspSchema }]), ColorsModule, SettingsModule],
