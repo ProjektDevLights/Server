39bb1432ad953edc1d4bcab34e13e1f2c937b7ef
Timo Peters <peters@teck-digital.de>
2021-06-09 20:05:49 +0200

    ehm ja

--- src/middlewares/esp.middleware.ts
+++ src/middlewares/esp.middleware.ts
@@ -4,7 +4,7 @@ import { Injectable, NestMiddleware } from "@nestjs/common";
 export class EspMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: () => void): void {
     if (!req.headers["user-agent"].toLowerCase().includes("arduino")) {
-      //throw new ForbiddenException("You aren't allowed to use this route!");
+      throw new ForbiddenException("You aren't allowed to use this route!");
     }
     next();
   }
